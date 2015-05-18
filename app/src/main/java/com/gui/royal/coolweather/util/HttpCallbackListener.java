defdd483965d8e8aab0cc14d85fd0b22b2feeef2
future302me <yang.gui302@gmail.com>
2015-05-18 09:44:04 +0800

    Revert "Revert "新增HttpUtil类，协助从服务器获取数据""
    This reverts commit d0f6abad637638bc9e2ea7747a59486bba03090f.

---
+++ app/src/main/java/com/gui/royal/coolweather/util/HttpCallbackListener.java
@@ -0,0 +1,10 @@
+package com.gui.royal.coolweather.util;
+
+/**回调服务器的返回结果
+ * Created by Jeremy on 2015/5/18.
+ */
+public interface HttpCallbackListener {
+    void onFinish(String response);
+
+    void onError (Exception e);
+}
